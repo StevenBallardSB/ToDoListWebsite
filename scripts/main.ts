f69ec6ee9b9cd8fd894e3ea800f548a0852a9848
STEVEN Ballard <ballard9576@students.cptc.edu>
2019-04-30 13:53:32 -0700

    Added ToDoItem Class

---
+++ scripts/main.ts
@@ -0,0 +1,19 @@
+
+class ToDoItem{
+    title:string;
+    description:string;
+    startDate:Date;
+    endDate:Date;
+    isComplete:boolean;
+    urgency:string;
+    //subTasks:Array<ToDoItem>;
+}
+
+let testItem = new ToDoItem();
+testItem.title = "Teach CPW 203";
+testItem.startDate= new Date("April 30, 2019");
+testItem.description = "Lecture advence JavaScript like a boss!";
+testItem.isComplete = true;
+if(testItem.isComplete){
+
+}
