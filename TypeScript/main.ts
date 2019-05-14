c820e9db3a0f09016e8e69a1ed086da8903ed0d8
STEVEN Ballard <ballard9576@students.cptc.edu>
2019-05-14 14:15:25 -0700

    Finished basic prototype adding/completing todos

--- TypeScript/main.ts
+++ TypeScript/main.ts
@@ -49,6 +49,25 @@ function processNewItem(){
     saveItem(item);
     notifyUser();
     clearForm();
+    displayToDo(item);
+}
+
+function displayToDo(item:ToDoItem){
+    let todoList = document.getElementById("todo-list");
+    let itemPar = document.createElement("p")
+    itemPar.innerText = item.title;
+    itemPar.setAttribute("data-description", item.description);
+    itemPar.onclick = toggleItemComplete;
+
+    todoList.appendChild(itemPar);
+}
+
+function toggleItemComplete(){
+    let currItem:HTMLElement = this;
+    currItem.classList.toggle("completed");
+    let title = currItem.innerText;
+    let description = currItem.getAttribute("data-description");
+    alert("You completed " + title + ":" + description);
 }
 
 function clearForm(){
